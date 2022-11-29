01b8888925f7daa1dc4fb558b37c2125f52d138e
masuda3969 <112703629+masuda3969@users.noreply.github.com>
2022-11-29 06:17:21 +0000

    AddGenre

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+
   devise_for :admins
   devise_for :customers
+  resources :genres
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
