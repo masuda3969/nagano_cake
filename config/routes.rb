05fc8aa3323b9255462d8c9909025d4908a66bec
masuda3969 <112703629+masuda3969@users.noreply.github.com>
2022-12-07 22:37:33 +0000

    AddItem

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :admins
   devise_for :customers
+namespace :admin do
   resources :genres
+  resources :items
+end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
